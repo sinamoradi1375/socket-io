0a199c86ccf216d0313a38eeac43e1ce26c60d77
sinamorad1375 <sina.moradii1375@gmail.com>
2022-02-06 12:45:09 +0330

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const port  = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+const getAPIAndEmit = socket => {
+    const response = new Date();
+
+    socket.emit("FromAPI", response);
+};
+
+let interval;
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+        getAPIAndEmit(socket);
+    }, 1000);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+        clearInterval(interval);
+    })
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
